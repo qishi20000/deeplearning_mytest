36e4f7dbce744e18aa8aec2afd9be79fa3982e88
qishi20000 <qishi20000@gmail.com>
2021-12-15 23:12:15 +0800

    完成15章计算

--- 15.py
+++ 15.py
@@ -9,8 +9,61 @@ def synthetic_data(w,b,num_examples):
     y += torch.normal(0,0.01,y.shape)
     return X,y.reshape((-1,1))
 
+#生成特征与标签数据
 true_w = torch.tensor([2,-3.4])
 true_b = 4.2
 features,labels = synthetic_data(true_w, true_b,1000)
 
-print(features)
+print('features:', features[0],'\nlabel:', labels[0])
+
+#将构造的数据转换为batch
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples))
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        batch_indices = torch.tensor(
+            indices[i: min(i + batch_size, num_examples)])
+        yield features[batch_indices], labels[batch_indices]
+
+batch_size = 10
+
+for X, y in data_iter(batch_size, features, labels):
+    print(X, '\n', y)
+    break
+
+#初始化模型值
+w = torch.normal(0, 0.01, size=(2,1), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+#定义模型
+def linreg(X, w, b):  
+    """线性回归模型。"""
+    return torch.matmul(X, w) + b
+
+#定义损失函数
+def squared_loss(y_hat, y):  
+    """均方损失。"""
+    return (y_hat - y.reshape(y_hat.shape)) ** 2 / 2
+
+#定义优化方法
+def sgd(params, lr, batch_size):  
+    """小批量随机梯度下降。"""
+    with torch.no_grad():
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_()
+#训练方法
+lr = 0.03
+num_epochs = 3
+net = linreg
+loss = squared_loss
+
+for epoch in range(num_epochs):
+    for X, y in data_iter(batch_size, features, labels):
+        l = loss(net(X, w, b), y)
+        l.sum().backward()
+        sgd([w, b], lr, batch_size)
+    with torch.no_grad():
+        train_l = loss(net(features, w, b), labels)
+        print(f'epoch {epoch + 1}, loss {float(train_l.mean()):f}')
